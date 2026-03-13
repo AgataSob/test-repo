a469958222180cf3ff701a1074301da007954eba
Agata <sobaga87@gmail.com>
2026-03-13 09:59:25 +0100

    Update Playwright configuration for improved test reporting and tracing

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,12 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+
+
+  reporter: [['html', { open: 'never' }]],
   use: {
-    trace: 'on-first-retry',
+    trace: 'on',
+    baseURL:'localhost:3000'
   },
 
   projects: [
